77cc0591a0f6725f31bc829beb71ecbb960b1e4a
WHitE-TITaN <dk488621@gmail.com>
2025-02-07 19:36:14 +0530

    added username to the score board!

--- CounterProgram/index.js
+++ CounterProgram/index.js
@@ -1,9 +1,12 @@
 // main js file
 window.alert("PAGE REFRESHED WELCOME !");
+let UserName = window.prompt("User Name pls!");
+console.log(UserName);
 console.log(`launch Successful`);
-let counetr = 1000;
+let counetr = 0;
 let running = true;
 
+document.getElementById("name").textContent = `${UserName} Score Counter!`;
 document.getElementById("Counter").textContent = counetr;
 
     document.getElementById("increment").onclick = function(){
