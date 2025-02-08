c9f392eac57eedc3e5ca4a9866131feb20bf772c
WHitE-TITaN <dk488621@gmail.com>
2025-02-09 00:52:36 +0530

    change in generator

--- RandomNumberGenerator/index.js
+++ RandomNumberGenerator/index.js
@@ -1,19 +1,25 @@
 //generating random number !
 
-let minimum = 0, maximum = 1_00_00_000;
-let randomGenerated = Math.floor(Math.random() * (maximum - minimum)) + minimum;
+let minimum = 1, maximum = 1_00_00_000;
+let randomGenerated = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+document.getElementById("OutPut").textContent = `${randomGenerated}`;
 
 
 console.log(randomGenerated);
+window.onload = function(){
+    window.onload = function() {
+        document.getElementById("OutPut").textContent = `${randomGenerated}`;
+    };
 
-window.onload = function() {
-    document.getElementById("OutPut").textContent = `${randomGenerated}`;
-};
+    document.getElementById("Submmit").onclick = function(){
+        minimum = Number(document.getElementById("LowRange").value);
+        maximum = Number(document.getElementById("HighRange").value);
 
-document.getElementById("Submmit").onclick = function(){
-    minimum = document.getElementById("LowRange").value;
-    maximum = document.getElementById("HighRange").value;
+        window.alert("Range Changed !");
+    }
 
-    randomGenerated = Math.floor(Math.random() * (maximum - minimum)) + minimum;
-    document.getElementById("OutPut").textContent = `${randomGenerated}`;
+    document.getElementById("newSuffel").onclick = function(){
+        randomGenerated = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+        document.getElementById("OutPut").textContent = `${randomGenerated}`;
+    }
 }
